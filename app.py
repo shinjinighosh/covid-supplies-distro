81ada544ff94b237e2c2be1a8aa5e6b4ec9f88f4
aalok-sathe <aalok.sathe@gmail.com>
2020-04-05 03:19:38 +0530

    flask init

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+import json
+
+from flask import Flask, url_for, request, render_template, json
+from argparse import ArgumentParser
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.config['APPLICATION_ROOT'] = '.'
 
 
 @app.route('/login')
 def login():
-    return
+    return render_template('index.html', stuff=render_template('login.html'))
+
+
+@app.route('/')
+def index():
+    return login()
+
+
+@app.route('/login/check', methods=['POST'])
+def login_check():
+    data = request.get_json()
+    print(data)
+
+    response = app.response_class(status=200, response=json.dumps({}),
+                                  mimetype='application/json')
+
+    return response
+
+if __name__ == "__main__":
+    app.run()
